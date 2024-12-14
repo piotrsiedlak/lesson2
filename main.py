8dacc7ece4ebdf6dfba7f3ea76d8fcb641abcf3d
piotrsiedlak <piotr.siedlak@microsoft.wsei.edu.pl>
2024-12-14 12:24:38 +0000

    added name recognition

--- main.py
+++ main.py
@@ -6,10 +6,14 @@ app = Flask(__name__)
 def hello_world():
     return '<p>Hello World!</p>'
 
+@app.route('/hello/<name>')
+def hello_name(name):
+   return 'Hello %s!' % name
+
 @app.route('/about')
 def about_us():
 	return 'Contact: me@mail.com Phone: 123-456-789'
 
 if __name__ == '__main__':
 
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
