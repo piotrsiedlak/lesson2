0e9e62f4d1512ea4f08b658ff8bf5122fc6a1a61
piotrsiedlak <piotr.siedlak@microsoft.wsei.edu.pl>
2025-01-08 17:52:14 +0000

    updated hello world subpage

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<p>Hello World!</p>'
+    return '<p>Hello Big World!</p>'
 
 @app.route('/hello/<name>')
 def hello_name(name):
